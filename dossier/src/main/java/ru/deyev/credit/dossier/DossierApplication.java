2842401f02273997c49568d2160b74b40819c9b5
MichaelD <michael.deyev@gmail.com>
2022-05-19 13:34:05 +0300

    init dossier application

---
+++ dossier/src/main/java/ru/deyev/credit/dossier/DossierApplication.java
@@ -0,0 +1,13 @@
+package ru.deyev.credit.dossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DossierApplication.class, args);
+    }
+
+}
