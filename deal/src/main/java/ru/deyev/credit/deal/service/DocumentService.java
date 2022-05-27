78adfa275897ac79d4b447bf391d5e208671d77f
MichaelD <michael.deyev@gmail.com>
2022-05-27 12:22:12 +0300

    add services

---
+++ deal/src/main/java/ru/deyev/credit/deal/service/DocumentService.java
@@ -0,0 +1,177 @@
+package ru.deyev.credit.deal.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.deyev.credit.deal.exception.DealException;
+import ru.deyev.credit.deal.model.Application;
+import ru.deyev.credit.deal.model.ApplicationStatus;
+import ru.deyev.credit.deal.model.ApplicationStatusHistoryDTO;
+import ru.deyev.credit.deal.model.Credit;
+import ru.deyev.credit.deal.model.CreditStatus;
+import ru.deyev.credit.deal.model.EmailMessage;
+import ru.deyev.credit.deal.repository.ApplicationRepository;
+import ru.deyev.credit.deal.repository.CreditRepository;
+
+import javax.persistence.EntityNotFoundException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static ru.deyev.credit.deal.model.ApplicationStatus.PREPARE_DOCUMENTS;
+import static ru.deyev.credit.deal.model.ApplicationStatusHistoryDTO.ChangeTypeEnum.MANUAL;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class DocumentService {
+
+    private DossierService dossierService;
+    private ApplicationRepository applicationRepository;
+    private CreditRepository creditRepository;
+
+    public void createDocumentsRequest(Long applicationId) {
+
+        Application application = applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new EntityNotFoundException("Application with id " + applicationId + " not found."));
+
+        if (application.getStatus() != ApplicationStatus.CC_APPROVED) {
+            throw new DealException("Application " + applicationId + " in status " + application.getStatus()
+                    + ", but should be in status " + ApplicationStatus.CC_APPROVED);
+        }
+
+        log.info("Sending create document request for application {}, to email {}",
+                application, application.getClient().getEmail());
+
+        dossierService.sendMessage(new EmailMessage()
+                .theme(EmailMessage.ThemeEnum.CREATE_DOCUMENT)
+                .applicationId(applicationId)
+                .address(application.getClient().getEmail()));
+    }
+
+    public void sendDocuments(Long applicationId) {
+        Application application = applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new EntityNotFoundException("Application with id " + applicationId + " not found."));
+
+        if (application.getStatus() != ApplicationStatus.CC_APPROVED) {
+            throw new DealException("Application " + applicationId + " in status " + application.getStatus()
+                    + ", but should be in status " + ApplicationStatus.CC_APPROVED);
+        }
+
+        List<ApplicationStatusHistoryDTO> statusHistory = application.getStatusHistory();
+        statusHistory.add(
+                new ApplicationStatusHistoryDTO()
+                        .status(PREPARE_DOCUMENTS)
+                        .time(LocalDateTime.now())
+                        .changeType(MANUAL));
+
+        applicationRepository.save(application
+                .setStatus(PREPARE_DOCUMENTS)
+                .setStatusHistory(statusHistory));
+
+        log.info("Sending send document request for application {}, to email {}",
+                application, application.getClient().getEmail());
+
+        dossierService.sendMessage(new EmailMessage()
+                .theme(EmailMessage.ThemeEnum.SEND_DOCUMENT)
+                .applicationId(applicationId)
+                .address(application.getClient().getEmail()));
+    }
+
+    public void signDocuments(Long applicationId) {
+        Application application = applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new EntityNotFoundException("Application with id " + applicationId + " not found."));
+
+        if (application.getStatus() != ApplicationStatus.DOCUMENT_CREATED) {
+            throw new DealException("Application " + applicationId + " in status " + application.getStatus()
+                    + ", but should be in status " + ApplicationStatus.DOCUMENT_CREATED);
+        }
+
+        Integer sesCode = generateSesCode();
+
+        applicationRepository.save(application.setSesCode(sesCode));
+
+        log.info("Sending send sign document request for application {}, to email {}",
+                application, application.getClient().getEmail());
+
+        dossierService.sendMessage(new EmailMessage()
+                .theme(EmailMessage.ThemeEnum.SEND_SES)
+                .applicationId(applicationId)
+                .address(application.getClient().getEmail()));
+
+
+    }
+
+    public void verifyCode(Long applicationId, Integer sesCode) {
+        Application application = applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new EntityNotFoundException("Application with id " + applicationId + " not found."));
+
+        if (application.getStatus() != ApplicationStatus.DOCUMENT_CREATED) {
+            throw new DealException("Application " + applicationId + " in status " + application.getStatus()
+                    + ", but should be in status " + ApplicationStatus.DOCUMENT_CREATED);
+        }
+
+        if (!sesCode.equals(application.getSesCode())) {
+            throw new DealException("For application " + applicationId + " wrong SES code " + sesCode
+                    + ". It should be in status " + application.getSesCode());
+        }
+
+        List<ApplicationStatusHistoryDTO> statusHistory = application.getStatusHistory();
+        statusHistory.add(new ApplicationStatusHistoryDTO()
+                .status(ApplicationStatus.DOCUMENT_SIGNED)
+                .time(LocalDateTime.now())
+                .changeType(ApplicationStatusHistoryDTO.ChangeTypeEnum.MANUAL));
+
+
+        applicationRepository.save(application.setStatus(ApplicationStatus.DOCUMENT_SIGNED)
+                .setStatusHistory(statusHistory)
+                .setSignDate(LocalDate.now()));
+
+        issueCredit(applicationId);
+    }
+
+    private void issueCredit(Long applicationId) {
+//        imitate long credit issuing action
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Application application = applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new EntityNotFoundException("Application with id " + applicationId + " not found."));
+
+        Long creditId = application.getCredit().getId();
+        Credit credit = creditRepository.findById(creditId)
+                .orElseThrow(() -> new EntityNotFoundException("Credit with id " + creditId + " not found."));
+
+        List<ApplicationStatusHistoryDTO> statusHistory = application.getStatusHistory();
+        statusHistory.add(new ApplicationStatusHistoryDTO()
+                .status(ApplicationStatus.CREDIT_ISSUED)
+                .time(LocalDateTime.now())
+                .changeType(ApplicationStatusHistoryDTO.ChangeTypeEnum.AUTOMATIC));
+
+
+        applicationRepository.save(application
+                .setStatus(ApplicationStatus.CREDIT_ISSUED)
+                .setStatusHistory(statusHistory));
+        creditRepository.save(credit.setCreditStatus(CreditStatus.ISSUED));
+
+        dossierService.sendMessage(new EmailMessage()
+                .theme(EmailMessage.ThemeEnum.CREDIT_ISSUED)
+                .applicationId(applicationId)
+                .address(application.getClient().getEmail()));
+
+        log.info("\n----------------------------------------" +
+                "\n-------------CONGRATULATION!------------" +
+                "\n--------CREDIT FOR APPLICATION {}-------" +
+                "\n----------------ISSUED------------------" +
+                "\n----------------------------------------", applicationId);
+
+    }
+
+    private Integer generateSesCode() {
+        int max = 9999;
+        int min = 1000;
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+}
---
+++ deal/src/main/java/ru/deyev/credit/deal/service/DossierService.java
@@ -0,0 +1,82 @@
+package ru.deyev.credit.deal.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import ru.deyev.credit.deal.model.EmailMessage;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class DossierService {
+
+    @Value("${custom.message.topic.finish-registration}")
+    private String FINISH_REGISTRATION_TOPIC;
+    @Value("${custom.message.topic.create-documents}")
+    private String CREATE_DOCUMENT_TOPIC;
+    @Value("${custom.message.topic.send-documents}")
+    private String SEND_DOCUMENT_TOPIC;
+    @Value("${custom.message.topic.send-ses}")
+    private String SEND_SES_TOPIC;
+    @Value("${custom.message.topic.credit-issued}")
+    private String CREDIT_ISSUED_TOPIC;
+    @Value("${custom.message.topic.application-denied}")
+    private String APPLICATION_DENIED_TOPIC;
+
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    private final ObjectMapper objectMapper;
+
+    public void sendMessage(EmailMessage message) {
+        String topic = evaluateTopic(message.getTheme());
+        String jsonMessage;
+        try {
+            jsonMessage = objectMapper.writeValueAsString(message);
+        } catch (JsonProcessingException e) {
+            log.warn("Cannot map message \"{}\" to JSON", message);
+            throw new RuntimeException(e);
+        }
+
+        log.info("Sending message \"{}\" to kafka topic \"{}\"", jsonMessage, topic);
+        kafkaTemplate.send(topic, jsonMessage);
+    }
+
+    private String evaluateTopic(EmailMessage.ThemeEnum theme) {
+        String topic = null;
+
+        switch (theme) {
+            case FINISH_REGISTRATION: {
+                topic = FINISH_REGISTRATION_TOPIC;
+                break;
+            }
+            case CREATE_DOCUMENT: {
+                topic = CREATE_DOCUMENT_TOPIC;
+                break;
+            }
+            case SEND_DOCUMENT: {
+                topic = SEND_DOCUMENT_TOPIC;
+                break;
+            }
+            case SEND_SES: {
+                topic = SEND_SES_TOPIC;
+                break;
+            }
+            case CREDIT_ISSUED: {
+                topic = CREDIT_ISSUED_TOPIC;
+                break;
+            }
+            case APPLICATION_DENIED: {
+                topic = APPLICATION_DENIED_TOPIC;
+                break;
+            }
+        }
+        return topic;
+    }
+
+
+}
