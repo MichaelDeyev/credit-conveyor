ee183006fbe12204286c4332e07c426469c6bc1e
MichaelD <michael.deyev@gmail.com>
2022-05-29 05:49:33 +0300

    add logging to MS gateway

---
+++ gateway/src/main/java/ru/deyev/credit/gateway/config/LogbookConfiguration.java
@@ -0,0 +1,14 @@
+package ru.deyev.credit.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.Logbook;
+
+@Configuration
+public class LogbookConfiguration {
+
+    @Bean
+    public Logbook logbook() {
+        return Logbook.create();
+    }
+}
